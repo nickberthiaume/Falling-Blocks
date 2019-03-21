65f67dd3a2fae6ed9582abacc995b2f75f0474c9
iwantmyfile5 <iwantmyfile5@users.noreply.github.com>
2019-03-20 21:18:50 -0400

    Initial commit

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+    public GameObject gameOverScreen;
+    public Text secondsSurvivedUI;
+    bool gameOver;
+
+	// Use this for initialization
+	void Start () {
+        FindObjectOfType<PlayerControl>().OnPlayerDeath += OnGameOver;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameOver)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene(0);
+            }
+            else if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+	}
+
+    void OnGameOver()
+    {
+        gameOverScreen.SetActive(true);
+        secondsSurvivedUI.text = Mathf.Round(Time.timeSinceLevelLoad).ToString();
+        gameOver = true;
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float speed = 7f;
+    public event System.Action OnPlayerDeath;
+
+    float screenHalfWidthInWolrdUnits;
+
+	// Use this for initialization
+	void Start () {
+        float halfPlayerWidth = transform.localScale.x / 2;
+        screenHalfWidthInWolrdUnits = Camera.main.aspect * Camera.main.orthographicSize + halfPlayerWidth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      float inputX = Input.GetAxisRaw("Horizontal");
+        float velocity = inputX * speed;
+
+        transform.Translate(Vector2.right * velocity * Time.deltaTime);
+        if(transform.position.x < -screenHalfWidthInWolrdUnits)
+        {
+            transform.position = new Vector2(screenHalfWidthInWolrdUnits, transform.position.y);
+        }
+        if (transform.position.x > screenHalfWidthInWolrdUnits)
+        {
+            transform.position = new Vector2(-screenHalfWidthInWolrdUnits, transform.position.y);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D triggerCollider)
+    {
+        if(triggerCollider.tag == "Falling Block")
+        {
+            if(OnPlayerDeath != null)
+            {
+                OnPlayerDeath();
+            }
+            Destroy(gameObject);
+        }
+    }
+}
